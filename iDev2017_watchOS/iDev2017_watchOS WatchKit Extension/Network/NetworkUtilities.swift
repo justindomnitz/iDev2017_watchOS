cdc35da0406165edb750401b566935dce65bdbc2
Justin Domnitz <justindomnitz@gmail.com>
2017-07-17 07:31:52 -0400

    Updated with openSSL built for physical Apple Watch architecture.

---
+++ iDev2017_watchOS/iDev2017_watchOS WatchKit Extension/Network/NetworkUtilities.swift
@@ -0,0 +1,81 @@
+//
+//  NetworkUtilities.swift
+//  iDev2017_watchOS WatchKit Extension
+//
+//  Created by Justin Domnitz on 7/11/17.
+//  Copyright © 2017 Lowyoyo, LLC. All rights reserved.
+//
+
+import Foundation
+
+public class NetworkUtilities: NSObject, URLSessionTaskDelegate {
+    
+    let openSSLPublicKeyLength = 767
+    let openSSLPublicKeyPrefix = "30 82 01 0A 02 82 01 01 00 "
+    
+    // MARK: Public Key Pinning Functions
+    
+    open func sendRequest(_ request:URLRequest, completion: @escaping (_ json: AnyObject?, _ data: Data?, _ response: HTTPURLResponse?, _ error: NSError?) -> Void) {
+        
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        let session = Foundation.URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue.main)
+        let _ = session.dataTask(with: request, completionHandler:{  (responseData, responseCode, error) -> Void in
+            
+            // to do
+            
+        })
+        .resume()
+    }
+    
+    open func urlSession(_ session: URLSession, task: URLSessionTask, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition,
+        URLCredential?) -> Void) {
+        
+        // SSL Pinning - START
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+            if let serverTrust = challenge.protectionSpace.serverTrust {
+                SecTrustEvaluate(serverTrust, nil)
+                
+                //to do
+                
+            }
+        }
+        // SSL Pinning - END
+        
+        let credential = URLCredentialStorage.shared.defaultCredential(for: challenge.protectionSpace)
+        var disposition = Foundation.URLSession.AuthChallengeDisposition.rejectProtectionSpace;
+        if challenge.previousFailureCount == 0 {
+            disposition = Foundation.URLSession.AuthChallengeDisposition.useCredential;
+        }
+        completionHandler(disposition, credential);
+    }
+    
+    //MARK: - Public Key Pinning Helper Functions
+    
+    private func showAlertControllerForGracefulExit(_ remoteVersionOfServerCertificateSummary:String) {
+        
+        // to do
+        
+    }
+    
+    private func publicKeyFromCertificate(_ certificateX509:UnsafeMutablePointer<X509>) -> String {
+        var publicKey = ""
+        
+        let publicKeyASN1 = X509_get0_pubkey_bitstr(certificateX509) //ASN1_BIT_STRING
+        if publicKeyASN1 != nil {
+            for i:Int32 in 0...(publicKeyASN1?.pointee.length)! {
+                publicKey += NSString.localizedStringWithFormat("%02x ", (publicKeyASN1?.pointee.data[Int(i)])!) as String
+            }
+        }
+        
+        //Make the string all upper case.
+        publicKey = publicKey.uppercased()
+        //Remove the openSSL public key prefix.
+        publicKey = publicKey.replacingOccurrences(of: openSSLPublicKeyPrefix, with: "")
+        //Remove the openSSL public key suffix.
+        if publicKey.characters.count > openSSLPublicKeyLength {
+            publicKey = publicKey.substring(to: publicKey.characters.index(publicKey.startIndex, offsetBy: Int(openSSLPublicKeyLength)))
+        }
+        return publicKey
+    }
+    
+}
---
+++ iDev2017_watchOS/iDev2017_watchOS WatchKit Extension/idev2017-watchOS-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  idev2017-watchOS-Bridging-Header.h
+//  iDev2017_watchOS WatchKit Extension
+//
+//  Created by Justin Domnitz on 7/11/17.
+//  Copyright © 2017 Lowyoyo, LLC. All rights reserved.
+//
+
+#ifndef idev2017_watchOS_Bridging_Header_h
+#define idev2017_watchOS_Bridging_Header_h
+
+#import "x509.h"
+
+#endif /* idev2017_watchOS_Bridging_Header_h */
