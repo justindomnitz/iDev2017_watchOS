664efb46cd38479c793a18f4bddfbf22560e8cc2
Justin Domnitz <justindomnitz@gmail.com>
2017-08-06 18:47:10 -0400

    Code clean up and initial complictions controller work.

--- iDev2017_watchOS WatchKit Extension/ComplicationController.swift
+++ iDev2017_watchOS WatchKit Extension/ComplicationController.swift
@@ -8,8 +8,30 @@
 
 import ClockKit
 
+struct ComplicationConstants {
+    static let CFASquareLogoCircularImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "ic_filter_1")!)
+    static let CFASquareLogoModularImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "ic_filter_1")!)
+    static let CFASquareLogoUtilitarianImageProvider = CLKImageProvider(onePieceImage: UIImage(named: "ic_filter_1")!)
+}
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
+    
+    let westerosCities = ["King's Landing",
+                          "Oldtown",
+                          "Lannisport",
+                          "Gulltown",
+                          "White Harbor"]
+    
+    let westerosRegions = ["Beyond the Wall",
+                           "The North",
+                           "The Iron Islands",
+                           "The Riverlands",
+                           "The Vale of Arryn",
+                           "The Westerlands",
+                           "The Crownlands",
+                           "The Reach",
+                           "The Stormlands",
+                           "Dorne"]
     
     // MARK: - Timeline Configuration
     
@@ -31,8 +53,59 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Timeline Population
     
+    // Unused types: CLKComplicationTemplateModularSmallRingImage, CLKComplicationTemplateModularSmallRingText,
+    //               CLKComplicationTemplateModularSmallSimpleImage, CLKComplicationTemplateModularSmallSimpleText,
+    //               CLKComplicationTemplateModularSmallStackImage, CLKComplicationTemplateModularSmallStackText
+    
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
+        //Because we're in getCurrentTimelineEntryForComplication, we don't have to care what date
+        //value is passed to the handler via the CLKComplicationTimelineEntry.  This value
+        //is ignored and whatever data we send is *always* displayed *right now*.
+        switch complication.family {
+        case .modularSmall:
+            if let template = getModularSmallTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            
+        case .modularLarge:
+            if let template = getModularLargeTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            
+        case .utilitarianSmall:
+            if let template = getUtilitarianSmallTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            
+        case .utilitarianSmallFlat:
+            if let template = getUtilitarianSmallFlatTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            break
+            
+        case .utilitarianLarge:
+            if let template = getUtilitarianLargeTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            
+        case .circularSmall:
+            if let template = getCircularSmallTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+            
+        case .extraLarge:
+            if let template = getExtraLargeTemplate() {
+                handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+                return
+            }
+        }
+        
         handler(nil)
     }
     
@@ -53,4 +126,67 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         handler(nil)
     }
     
+    func getModularLargeTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateModularLargeColumns()
+        template.row1Column1TextProvider = CLKSimpleTextProvider(text: "King's"   , shortText: "King's" )
+        template.row1Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""     )
+        template.row2Column1TextProvider = CLKSimpleTextProvider(text: "Landing"  , shortText: "Land"   )
+        template.row2Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""     )
+        template.row3Column1TextProvider = CLKSimpleTextProvider(text: ""         , shortText: "ing"  )
+        template.row3Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""     )
+        template.row1ImageProvider = ComplicationConstants.CFASquareLogoModularImageProvider
+        template.column2Alignment = .trailing
+        return template
+    }
+    
+    func getModularSmallTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateModularSmallColumnsText()
+        template.row1Column1TextProvider = CLKSimpleTextProvider(text: "King's"   , shortText: "King's" )
+        template.row1Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""       )
+        template.row2Column1TextProvider = CLKSimpleTextProvider(text: "Landing"  , shortText: "Land"   )
+        template.row2Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""       )
+        return template
+    }
+    
+    func getUtilitarianLargeTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateUtilitarianLargeFlat()
+        template.textProvider = CLKSimpleTextProvider(text:      "King's Landing",
+                                                      shortText: "King's Land")
+        template.imageProvider = ComplicationConstants.CFASquareLogoUtilitarianImageProvider
+        return template
+    }
+    
+    func getUtilitarianSmallTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateUtilitarianSmallFlat()
+        template.textProvider = CLKSimpleTextProvider(text: "King's", shortText: "Ki")
+        return template
+    }
+    
+    func getUtilitarianSmallFlatTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateUtilitarianSmallFlat()
+        template.textProvider = CLKSimpleTextProvider(text: "King's", shortText: "Ki")
+        return template
+    }
+    
+    func getCircularSmallTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateCircularSmallRingImage()
+        template.imageProvider = ComplicationConstants.CFASquareLogoCircularImageProvider
+        template.ringStyle = .closed
+        return template
+    }
+    
+    func getExtraLargeTemplate() -> CLKComplicationTemplate? {
+        let template = CLKComplicationTemplateModularLargeColumns()
+        template.row1Column1TextProvider = CLKSimpleTextProvider(text: "King's"   , shortText: "King's" )
+        template.row1Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""       )
+        template.row2Column1TextProvider = CLKSimpleTextProvider(text: "Landing"  , shortText: "Land"   )
+        template.row2Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""       )
+        template.row3Column1TextProvider = CLKSimpleTextProvider(text: ""         , shortText: "ing"    )
+        template.row3Column2TextProvider = CLKSimpleTextProvider(text: ""         , shortText: ""       )
+        template.row1ImageProvider = ComplicationConstants.CFASquareLogoModularImageProvider
+        template.column2Alignment = .trailing
+        return template
+    }
+    
 }
+
--- iDev2017_watchOS WatchKit Extension/TweetInterfaceController.swift
+++ iDev2017_watchOS WatchKit Extension/TweetInterfaceController.swift
@@ -21,73 +21,97 @@ class TweetInterfaceController: WKInterfaceController {
     //MARK: - Lifecycle Methods
     
     override func awake(withContext context: Any?) {
+        print("TweetInterfaceController - \(#function)")
+
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-        print("TweetInterfaceController - awake")
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+        print("TweetInterfaceController - \(#function)")
 
-        print("TweetInterfaceController - willActivate")
-        
+        super.willActivate()
+    }
+    
+    override func didAppear() {
+        print("TweetInterfaceController - \(#function)")
+
+        clearTwitterData()
         getTwitterData()
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    override func willDisappear() {
+        print("TweetInterfaceController - \(#function)")
+
+        clearTwitterData()
+    }
     
-        print("TweetInterfaceController - didDeactivate")
+    override func didDeactivate() {
+        print("TweetInterfaceController - \(#function)")
+
+        super.didDeactivate()
     }
     
     //MARK: - Helper Methods
     
+    func clearTwitterData() {
+        tweetTable.setHidden(true)
+    }
+    
     func getTwitterData() {
         
-        tweetTable.setHidden(true)
         loadingLabel.setHidden(false)
         startActivityIndicator()
         
-        var validTweets = [Tweet]()
-        
-        let tweet1 = Tweet()
-        var user1 = User()
-        tweet1?.text = "Winter is coming!"
-        user1.screenName = "John Snow"
-        tweet1?.user = user1
-        if let tweet1 = tweet1 {
-            validTweets.append(tweet1)
+        //Static data.
+        delay(1) {
+            var validTweets = [Tweet]()
+            
+            let tweet1 = Tweet()
+            var user1 = User()
+            tweet1?.text = "Winter is coming!"
+            user1.screenName = "John Snow"
+            tweet1?.user = user1
+            if let tweet1 = tweet1 {
+                validTweets.append(tweet1)
+            }
+            let tweet2 = Tweet()
+            var user2 = User()
+            tweet2?.text = "I'm the queen of dragons!"
+            user2.screenName = "Daenerys Targaryen"
+            tweet2?.user = user2
+            if let tweet2 = tweet2 {
+                validTweets.append(tweet2)
+            }
+            
+            self.loadingLabel.setHidden(true)
+            self.tweetTable.setHidden(false)
+            self.stopActivityIndicator()
+            self.tweetTable.setNumberOfRows(validTweets.count, withRowType: "tweetRowController")
+            for (index, tweet) in validTweets.enumerated() {
+                if let row = self.tweetTable.rowController(at: index) as? TweetRowController {
+                    row.tweetLabel.setText(tweet.text)
+                    row.tweetImage.setImage(UIImage(named: "DaenerysTargaryen"))
+                }
+            }
         }
-        let tweet2 = Tweet()
-        var user2 = User()
-        tweet2?.text = "I'm the queen of dragons!"
-        user2.screenName = "Daenerys Targaryen"
-        tweet2?.user = user2
-        if let tweet2 = tweet2 {
-            validTweets.append(tweet2)
-        }
         
-        delay(2) {
-        
-//        TwitterInterface().requestTwitterSearchResults(hashtag, completion: { (tweets, error) -> Void in
-//            if error == nil {
-//                DispatchQueue.main.async {
+        //Data from server.
+        /*
+        TwitterInterface().requestTwitterSearchResults(hashtag, completion: { (tweets, error) -> Void in
+            if error == nil {
+                DispatchQueue.main.async {
 
                     self.loadingLabel.setHidden(true)
                     self.tweetTable.setHidden(false)
                     self.stopActivityIndicator()
                     
-//                    if let validTweets = tweets {
-                        //self.tweets.removeAll()
-                        //self.tweets.insert(validTweets, at: 0)
+                    if let validTweets = tweets {
+                        self.tweets.removeAll()
+                        self.tweets.insert(validTweets, at: 0)
                         self.tweetTable.setNumberOfRows(validTweets.count, withRowType: "tweetRowController")
                         for (index, tweet) in validTweets.enumerated() {
                             if let row = self.tweetTable.rowController(at: index) as? TweetRowController {
                                 row.tweetLabel.setText(tweet.text)
-                                row.tweetImage.setImage(UIImage(named: "DaenerysTargaryen"))
 //                                if let profileImageURL = tweet.user.profileImageURL {
 //                                    if let imageData = try? Data(contentsOf: profileImageURL as URL) { // TO-DO: blocks main thread!
 //                                        row.tweetImage.setImage(UIImage(data: imageData))
@@ -95,15 +119,15 @@ class TweetInterfaceController: WKInterfaceController {
 //                                }
                             }
                         }
-//                    }
-//                }
-//            }
-//            else {
-//                print(error.debugDescription)
-//            }
-//        })
-            
-        }
+                    }
+                }
+            }
+            else {
+                print(error.debugDescription)
+            }
+        })
+        */
+        
     }
     
     func startActivityIndicator() {
